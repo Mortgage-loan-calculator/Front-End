31e7e2a9eb0ec198e65555c3d6d661371fde2b92
Evelina <venckute.evelina1.com>
2023-04-11 15:52:19 +0300

    Added Form and Home components and Button which doesnt work

--- Mortgage-loan-calculator/src/app/app-routing.module.ts
+++ Mortgage-loan-calculator/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorFormComponent } from './calculator-form/calculator-form.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+
+  { path: 'calculator', component: CalculatorFormComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
