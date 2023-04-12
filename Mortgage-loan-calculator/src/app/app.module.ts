d5e153ee3c46de1ae31f0471ed075ef5708b1dc5
lmiskelis <lukas.miskelis@gmail.com>
2023-04-12 13:48:28 +0300

    movingMaterialDependencys

--- Mortgage-loan-calculator/src/app/app.module.ts
+++ Mortgage-loan-calculator/src/app/app.module.ts
@@ -7,17 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CalculatorFormComponent } from './calculator-form/calculator-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input'
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSelectModule} from '@angular/material/select';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { PopupFormComponent } from './popup-form/popup-form.component';
 import { MaterialModule } from './material/material.module';
@@ -35,16 +26,7 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatFormFieldModule,
     NgxSliderModule,
-    MatSlideToggleModule,
-    MatSelectModule
-    MatPaginatorModule,
-    MatTableModule,
-    MatTooltipModule,
-    MatToolbarModule,
-    MatIconModule,
     HttpClientModule,
     MaterialModule
   ],
