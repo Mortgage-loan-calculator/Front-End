373f16c68ee60aaab586ad1971e19c98a63b0be9
lmiskelis <lukas.miskelis@gmail.com>
2023-04-18 10:02:31 +0300

    pie chart with variables withtout delay

--- Mortgage-loan-calculator/src/app/pie-chart/pie-chart.component.ts
+++ Mortgage-loan-calculator/src/app/pie-chart/pie-chart.component.ts
@@ -1,5 +1,14 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectorRef,
+} from '@angular/core';
 import Chart from 'chart.js/auto';
+
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
@@ -8,40 +17,75 @@ import Chart from 'chart.js/auto';
 export class PieChartComponent {
   @ViewChild('chartCanvas') chartCanvas?: ElementRef;
 
+  private _loanAmount: number = 2;
+  private _intrestPaid: number = 3;
+  private _agreementFee: number = 1;
+
   public chart: any;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
   ngOnInit(): void {
     this.createChart();
   }
 
-  constructor() {}
-
-  animateChart() {
+  ngOnChanges(changes: SimpleChanges) {
     if (this.chart) {
-      this.chart.options.animation = {
-        onComplete: () => {
-          this.chart.options.animation = false;
-        },
-      };
-      this.chart.update();
+      this.updateChart();
     }
   }
+
+  @Input() set loanAmount(value: number) {
+    this._loanAmount = value;
+    this.updateChart();
+  }
+
+  get loanAmount(): number {
+    return this._loanAmount;
+  }
+
+  @Input() set intrestPaid(value: number) {
+    this._intrestPaid = value;
+    this.updateChart();
+  }
+
+  get intrestPaid(): number {
+    return this._intrestPaid;
+  }
+
+  @Input() set agreementFee(value: number) {
+    this._agreementFee = value;
+    this.updateChart();
+  }
+
+  get agreementFee(): number {
+    return this._agreementFee;
+  }
+
+  updateChart() {
+    this.chart.data.datasets[0].data = [
+      this._loanAmount,
+      this._intrestPaid,
+      this._agreementFee,
+    ];
+    this.chart.update();
+  }
+
   createChart() {
     this.chart = new Chart('MyChart', {
       type: 'pie',
-
       data: {
         labels: ['Loan Amount', 'Total interest paid', 'Agreement fee'],
         datasets: [
           {
             label: 'Money',
-            data: [300, 240, 100],
+            data: [this._loanAmount, this._intrestPaid, this._agreementFee],
             backgroundColor: ['blue', 'red', 'green'],
             hoverOffset: 4,
           },
         ],
       },
       options: {
-        // responsive: false,
         aspectRatio: 2,
         plugins: {
           legend: {
