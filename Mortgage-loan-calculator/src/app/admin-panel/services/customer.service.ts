afb5a186e7c345f07c3134c0255b1656ca1cd256
linas159 <linas.mintautiskis@gmail.com>
2023-04-20 12:22:08 +0300

    added server api links

--- Mortgage-loan-calculator/src/app/admin-panel/services/customer.service.ts
+++ Mortgage-loan-calculator/src/app/admin-panel/services/customer.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class CustomerService {
   constructor(private http: HttpClient) {}
   customerUrl =
-    'http://localhost:8080/customers';
+    'https://mortgage-loan-calculator-back-end.onrender.com/customers';
 
   public getCustomer(): Observable<Customer[]> {
     return this.http.get<Customer[]>(this.customerUrl);
