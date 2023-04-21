395d3dc7c2e922b2846d426619b304fee838bc00
tkazevicius <t.kazevicius@gmail.com>
2023-04-21 12:21:22 +0300

    Added monthly payment form logic

---
+++ Mortgage-loan-calculator/src/app/monthly-payment/monthly-payment-dto.ts
@@ -0,0 +1,12 @@
+export interface MonthlyPaymentDto {
+    id?: any;
+    dealAmount?: any;
+    downPayment?: any;
+    loanPeriod?: any;
+}
+
+export interface MonthlyPaymentResultsDto{
+    estimatedMonthlyPayment?: any;
+    maxMonthlyPayment?: any;
+
+}
