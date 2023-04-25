181664d0b63e3bebf52e418b7e46d69fdd301ac3
Evelina <venckute.evelina1.com>
2023-04-25 13:45:17 +0300

    fix

--- Mortgage-loan-calculator/src/app/admin-panel/services/customer.service.ts
+++ Mortgage-loan-calculator/src/app/admin-panel/services/customer.service.ts
@@ -10,6 +10,7 @@ export class CustomerService {
   constructor(private http: HttpClient) {}
   customerUrl =
     'https://mortgage-loan-calculator-back-end.onrender.com/customers';
+    // 'http://localhost:8090/customers';
 
   public getCustomer(): Observable<Customer[]> {
     return this.http.get<Customer[]>(this.customerUrl);
